12a1bee0bc0ecd57425cdc8941346f87bdae8726
SimpleGamerDK <65606088+SimpleGamerDK@users.noreply.github.com>
2022-06-03 23:17:53 +0200

    Add files via upload

---
+++ sg-washhands/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.ProgressBarLabel = 'Washing Hands' -- The text shown on the progress bar
+Config.ProgressBarLength = 5 -- Time it takes to wash your hands in seconds
+Config.NotifyLabel = "You have washed your hands clean" -- The text shown in the notify
+Config.TargetLabel = 'Wash Hands' -- The text shown when using Third Eye
