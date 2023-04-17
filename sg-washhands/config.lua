ba99835d461e4661fb89203ea00c40cd01f1c4cb
SimpleGamerDK <65606088+SimpleGamerDK@users.noreply.github.com>
2023-04-17 13:04:11 +0200

    Delete sg-washhands directory

--- sg-washhands/config.lua
+++
@@ -1,6 +0,0 @@
-Config = {}
-
-Config.ProgressBarLabel = 'Washing Hands' -- The text shown on the progress bar
-Config.ProgressBarLength = 5 -- Time it takes to wash your hands in seconds
-Config.NotifyLabel = "You have washed your hands clean" -- The text shown in the notify
-Config.TargetLabel = 'Wash Hands' -- The text shown when using Third Eye
