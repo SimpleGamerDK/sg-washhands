e7a54f5e3c974bf67169fd4b32811631eb1f1861
SimpleGamerDK <65606088+SimpleGamerDK@users.noreply.github.com>
2023-04-18 13:38:54 +0200

    Delete sg-washhands directory

--- sg-washhands/locals.lua
+++
@@ -1,9 +0,0 @@
-Locals = {}
-
-Locals = {
-    ['en'] = {
-        TargetLabel = "Wash Hands",
-        NotifyLabel = "You have washed your hands clean",
-        ProgbarLabel = "Washing Hands"
-    }
-}
